dff0836b0da4b178f618a76e85cc8f1e82a286ef
ridozaen <ridozaen@gmail.com>
2018-04-20 02:23:11 +0700

    added file exercises4

---
+++ Exercises4_graduates.js
@@ -0,0 +1,101 @@
+function graduates (students) {
+    var gradList = {};
+    // create object gradList with properties empty;
+    for (var i = 0; i < students.length; i++){
+        gradList[students[i].class] = '';
+    }
+
+    for (var key in gradList){
+        var arrGrad = [];
+        for (var i = 0; i < students.length; i++){
+            var studentGrad = {};
+            if (key === students[i].class){
+                if (students[i].score >= 75){
+                    studentGrad.name = students[i].name;
+                    studentGrad.score = students[i].score;
+                    arrGrad.push(studentGrad);
+                }
+            }
+        }
+        gradList[key] = arrGrad;
+    }
+    return gradList;
+  }
+  
+  console.log(graduates([
+    {
+      name: 'Dimitri',
+      score: 90,
+      class: 'foxes'
+    },
+    {
+      name: 'Alexei',
+      score: 85,
+      class: 'wolves'
+    },
+    {
+      name: 'Sergei',
+      score: 74,
+      class: 'foxes'
+    },
+    {
+      name: 'Anastasia',
+      score: 78,
+      class: 'wolves'
+    }
+  ]));
+  
+  // {
+  //   foxes: [
+  //     { name: 'Dimitri', score: 90 }
+  //   ],
+  //   wolves: [
+  //     { name: 'Alexei' , score: 85 },
+  //     { name: 'Anastasia', score: 78 }
+  //   ]
+  // }
+  
+  console.log(graduates([
+    {
+      name: 'Alexander',
+      score: 100,
+      class: 'foxes'
+    },
+    {
+      name: 'Alisa',
+      score: 76,
+      class: 'wolves'
+    },
+    {
+      name: 'Vladimir',
+      score: 92,
+      class: 'foxes'
+    },
+    {
+      name: 'Albert',
+      score: 71,
+      class: 'wolves'
+    },
+    {
+      name: 'Viktor',
+      score: 80,
+      class: 'tigers'
+    }
+  ]));
+  
+  // {
+  //   foxes: [
+  //     { name: 'Alexander', score: 100 },
+  //     { name: 'Vladimir', score: 92 }
+  //   ],
+  //   wolves: [
+  //     { name: 'Alisa', score: 76 },
+  //   ],
+  //   tigers: [
+  //     { name: 'Viktor', score: 80 }
+  //   ]
+  // }
+  
+  
+  console.log(graduates([])); //{}
+  
