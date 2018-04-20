62388a6eb15aea3276beb2672fed4a9298814e27
ridozaen <ridozaen@gmail.com>
2018-04-20 08:53:43 +0700

    modified exercises8 to prevent error when string doesn't exist

--- Exercises8_cariPelaku.js
+++ Exercises8_cariPelaku.js
@@ -1,5 +1,5 @@
 function cariPelaku(str) {
-    return str.match(/abc/g).length;
+    return (str)? str.match(/abc/g).length : '';
   }
   
   // TEST CASES
@@ -8,4 +8,5 @@ function cariPelaku(str) {
   console.log(cariPelaku('bcabcac')); // 1
   console.log(cariPelaku('abcabcabc')); // 3
   console.log(cariPelaku('babcbacabc')); // 2
+  console.log(cariPelaku(''));
   
